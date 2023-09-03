03205a5096a719d9bcbefa16877b37043e7e1964
wwd <1292042074@qq.com>
2023-09-03 14:17:50 +0800

    fix:incorporating DoctorInfo and Header into the doctor interface

---
+++ src/views/PatientLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-container>
+    <el-aside width="256px">
+      <SideBar />
+    </el-aside>
+    <router-view></router-view>
+  </el-container>
+  <Header />
+</template>
+
+<script>
+import SideBar from "@/components/SideBar.vue";
+import Header from "@/components/Header.vue";
+export default {
+  components: {
+    SideBar,
+    Header,
+  },
+};
+</script>
+
+<style scoped>
+</style>
