22c7bd92d8b01226c375697b79a56186a41df275
Lingjun Mao <78578780+mao1207@users.noreply.github.com>
2023-09-04 16:36:17 +0800

    feat: Use sessionStorage

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -153,9 +153,9 @@ export default {
               path: AfterLogin[this.role_num],
             });
 
-            //hcr更改，存储用户账号信息到user.js
-            userInfo.state.userID = this.loginForm.username;
-            userInfo.state.role = this.role_num;
+            sessionStorage.setItem("userID", this.loginForm.username)
+            sessionStorage.setItem("role", this.role_num)
+            
           } else {
             // 登录失败
             console.error("登录失败");
